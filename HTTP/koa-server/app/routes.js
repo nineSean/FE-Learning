9a69ca9712b055985aaabac23e4eb20548bd7f4d
nineSean <ninesean1989@gmail.com>
2017-01-11 12:51:25 +0800

    first commit

---
+++ HTTP/koa-server/app/routes.js
@@ -0,0 +1,16 @@
+module.exports = function(app) {
+	var Router 		= require('koa-router'),
+		indexCtrl 	= require('./controllers/index');
+
+	var router = new Router();
+
+	router
+		.get('/', indexCtrl.index)
+        .get('/index2', indexCtrl.index2)
+		.get('/a',indexCtrl.a)
+		.get('/data.js', indexCtrl.data)
+        .post('/', indexCtrl.post)
+        .post('/post2', indexCtrl.post2)
+
+	app.use(router.middleware());
+};
---
+++ HTTP/koa-server/index.js
@@ -0,0 +1,29 @@
+var koa = require('koa')
+var path = require('path')
+var views = require('koa-views')
+var serve = require('koa-static')
+var bodyParser = require('koa-body-parser')
+
+
+var app = module.exports = koa()
+
+app.use(views('app/views', {
+  map: {
+    hbs: 'handlebars',
+    html: 'handlebars',
+  }
+}))
+
+app.use(bodyParser());
+app.use(function * (next) {
+  console.log('有人访问了 ' + this.request.host + this.request.url) //打印出被访问的路径
+  //this.set('Access-Control-Allow-Origin', 'http://a.com')  // 设置响应头
+  yield next
+})
+
+app.use(require('koa-static')('./static'));  // 开启静态文件的访问
+
+require('./app/routes')(app);
+
+app.listen(8080)
+console.log('http://127.0.0.1:8080')
