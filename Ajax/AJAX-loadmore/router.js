a42318ea1444f69b4b1c6078066bb3b99b89f35c
nineSean <vipatlx@qq.com>
2017-02-18 02:45:50 +0800

    modify loadmore.html

--- Ajax/AJAX-loadmore/router.js
+++ Ajax/AJAX-loadmore/router.js
@@ -4,8 +4,8 @@
 
 
 app.get('/loading', function (req, res) {
-    var start = req.query.start,
-        len = req.query.length,
+    var start = req.query.start - 0,
+        len = req.query.length - 0,
         data = [];
 
     for(var i = start + 1; i<= start + len; i++){
